ba2d93b8c1934efbbf7ea8a89f6d544202b53e14
ispan <ispan@DESKTOP-PAKSETB>
2022-10-24 17:32:27 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace WindowsFormsApp5
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		/// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 設計工具產生的程式碼
+
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+		/// 這個方法的內容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(76, 77);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(53, 12);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "輸入列數";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(166, 74);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 22);
+			this.textBox1.TabIndex = 1;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(324, 70);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox2.Location = new System.Drawing.Point(78, 146);
+			this.textBox2.Multiline = true;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.textBox2.Size = new System.Drawing.Size(385, 196);
+			this.textBox2.TabIndex = 3;
+			this.textBox2.WordWrap = false;
+			// 
+			// label2
+			// 
+			this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label2.Location = new System.Drawing.Point(88, 372);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(405, 184);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "label2";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(417, 70);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 5;
+			this.button2.Text = "button2";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(524, 70);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 6;
+			this.button3.Text = "button3";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(830, 565);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			label2.Text =string.Empty;
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			int? rows = getrows();
+			
+			if(rows.HasValue == false)
+			{
+				MessageBox.Show("請輸入數列");
+				return;
+			}
+			if(rows.Value <= 0)
+			{
+				MessageBox.Show("列數必須大於0");
+				return ;
+			}
+			string stars = Generatestars(rows.Value);
+
+			textBox2.Text = stars;
+			label2.Text = stars;
+		}
+
+		private string Generatestars(int rows)
+		{
+			string result =string.Empty;
+			for(int i = 1; i <=rows ; i++)
+			{
+				result += new string('*', i) + "\r\n";
+			}
+			return result;
+		}
+
+		private string Generatestars1(int rows)
+		{
+			string result = string.Empty;
+			for (int i = 1; i <= rows; i++)
+			{
+				result += new string(' ', rows-i)+new string('*', i) + "\r\n";
+			}
+			return result;
+		}
+		private string Generatestars2(int rows)
+		{
+			string result = string.Empty;
+			for (int i = 1; i <=rows; i++)
+			{
+				result += new string(' ', (rows - i)*2)+new string('*', (i*2)-1) + "\r\n";
+			}
+			return result;
+		}
+
+		private int? getrows()
+		{
+			string input =textBox1.Text;
+			bool isint = int.TryParse(input, out int rows);
+			if (isint)
+			{
+				return rows;
+			}
+			else
+			{
+				return null;
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			int? rows = getrows();
+
+			if (rows.HasValue == false)
+			{
+				MessageBox.Show("請輸入數列");
+				return;
+			}
+			if (rows.Value <= 0)
+			{
+				MessageBox.Show("列數必須大於0");
+				return;
+			}
+			string stars = Generatestars1(rows.Value);
+
+			textBox2.Text = stars;
+			label2.Text = stars;
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			int? rows = getrows();
+
+			if (rows.HasValue == false)
+			{
+				MessageBox.Show("請輸入數列");
+				return;
+			}
+			if (rows.Value <= 0)
+			{
+				MessageBox.Show("列數必須大於0");
+				return;
+			}
+			string stars = Generatestars2(rows.Value);
+
+			textBox2.Text = stars;
+			label2.Text = stars;
+		}
+	}
+}
